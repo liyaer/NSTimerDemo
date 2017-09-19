d94fc6870a590421b6a82644bb042850fb48228b
DuWenLiang <739285514@qq.com>
2017-09-19 16:52:46 +0800

    Initial Commit

---
+++ NSTimerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSTimerDemo
+//
+//  Created by 杜文亮 on 2017/9/19.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
